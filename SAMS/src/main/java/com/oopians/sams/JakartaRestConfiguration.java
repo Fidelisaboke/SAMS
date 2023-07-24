f187ee42d8119b9a64afd988a6ccb63bedc7b305
fidelisaboke <fidelisaboke@gmail.com>
2023-07-24 15:35:50 +0300

    Modified the config files

--- SAMS/src/main/java/com/oopians/sams/JakartaRestConfiguration.java
+++ SAMS/src/main/java/com/oopians/sams/JakartaRestConfiguration.java
@@ -1,7 +1,9 @@
 package com.oopians.sams;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
